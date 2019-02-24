a05a93a0972db366265314ffa115d1c4654ff7ca
rexzheng <dev@rex.cc>
2019-02-24 15:09:56 +0800

    certs use instance pattern

--- src/certs.js
+++ src/certs.js
@@ -1,38 +1,63 @@
 const assert = require('assert');
+const tls = require('tls');
 const certutil = require('./certutil');
 const LRU = require('lru-cache');
 
 const ROOT_NAME = 'MUSES_ROOT_CA';
 const ROOT_DAYS = 365 * 5;
 const DEFAULT_DAYS = 14;
-const cache = new LRU();
-let root = null;
 
-function setRoot(cert) {
-  assert(cert instanceof certutil.Cert, 'invalid cert');
-
-  root = cert;
-}
-
-function getRoot() {
-  if (!root) {
-    root = certutil.makeCa(ROOT_NAME, ROOT_DAYS);
-  }
-  return root;
-}
-
-function get(commonName) {
-  assert(commonName && typeof commonName === 'string', 'invalid commonName');
-
-  let cert = cache.get(commonName);
-  if (!cert) {
-    cert = getRoot().issue(commonName, DEFAULT_DAYS);
-    cache.set(commonName, cert);
+class CertsCache {
+  constructor() {
+    Object.defineProperty(this, '_cache', {
+      value: new LRU()
+    });
+    if (arguments.length > 0) {
+      this.setRoot(...arguments);
+    } else {
+      this._root = null;
+    }
   }
 
-  return cert;
+  /**
+   * set root CA
+   * @param {Cert} cert
+   *
+   * set root CA by pem
+   * @param {string} certPem
+   * @param {string} keyPem
+   */
+  setRoot(cert) {
+    if (arguments.length === 1) {
+      assert(cert instanceof certutil.Cert, 'invalid cert');
+      this._root = cert;
+    } else if (arguments.length === 2) {
+      let certPem = arguments[0];
+      let keyPem = arguments[1];
+      this._root = certutil.loadPem(certPem, keyPem);
+    } else {
+      throw new Error('invalid arguments');
+    }
+  }
+
+  getRoot() {
+    if (!this._root) {
+      this._root = certutil.makeCa(ROOT_NAME, ROOT_DAYS);
+    }
+    return this._root;
+  }
+
+  get(commonName) {
+    assert(commonName && typeof commonName === 'string', 'invalid commonName');
+
+    let ctx = this._cache.get(commonName);
+    if (!ctx) {
+      let cert = this.getRoot().issue(commonName, DEFAULT_DAYS);
+      ctx = tls.createSecureContext(cert.pem());
+      this._cache.set(commonName, ctx);
+    }
+
+    return ctx;
+  }
 }
 
-exports.get = get;
-exports.setRoot = setRoot;
-exports.getRoot = getRoot;
